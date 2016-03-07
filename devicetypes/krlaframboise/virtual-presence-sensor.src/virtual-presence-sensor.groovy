3a93cc66fe738a035eb71c596528cad225d2fd3e
Kevin LaFramboise <klaframboise@comcast.net>
2016-03-07 16:11:42 -0500

    - Made force commands always change state.

--- devicetypes/krlaframboise/virtual-presence-sensor.src/virtual-presence-sensor.groovy
+++ devicetypes/krlaframboise/virtual-presence-sensor.src/virtual-presence-sensor.groovy
@@ -1,23 +1,39 @@
 /**
- *  Copyright 2016 Kevin LaFramboise
+ *  Virtual Presence Sensor v 1.1
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
+ *  Capabilities:
+ *    Presence Sensor
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *  Author: 
+ *    Kevin LaFramboise (krlaframboise)
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
+ *  Changelog:
  *
+ *    1.1 (03/06/2016)
+ *      -	Made force commands always change state.
+ *
+ *  Licensed under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a 
+ *  copy of the License at:
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in
+ *  writing, software distributed under the License is
+ *  distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ *  OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing
+ *  permissions and limitations under the License.
  */
 metadata {
 	definition (name: "Virtual Presence Sensor", namespace: "krlaframboise", author: "Kevin LaFramboise") {
 		capability "Presence Sensor"
 		capability "Sensor"
 		capability "Actuator"
-command "forcePresent"
-command "forceNotPresent"
+		
+		command "forcePresent"
+		command "forceNotPresent"
 	}
 
 	simulator {
@@ -28,8 +44,16 @@ command "forceNotPresent"
 	tiles(scale: 2) {
 	multiAttributeTile(name:"presence", type: "generic", width: 6, height: 3, canChangeIcon: true){
 			tileAttribute ("presence", key: "PRIMARY_CONTROL") {
-				attributeState( "present", label:'Present', backgroundColor:"#99c2ff", labelIcon:"st.presence.tile.mobile-present", action: "forceNotPresent")
-				attributeState("not present", label:'Not Present', backgroundColor: "#cccccc", labelIcon:"st.presence.tile.mobile-present", action: "forcePresent")			
+				attributeState( "present", 
+					label:'Present', 
+					backgroundColor:"#99c2ff", 
+					labelIcon:"", 
+					action: "forceNotPresent")
+				attributeState("not present", 
+					label:'Not Present', 
+					backgroundColor: "#cccccc", 
+					labelIcon:"", 
+					action: "forcePresent")			
 			}
 		}				
 		main "presence"
@@ -38,10 +62,10 @@ command "forceNotPresent"
 }
 
 def forcePresent() {
-	sendEvent(name: "presence", value: "present")
+	sendEvent(name: "presence", value: "present", isStateChange: true)
 }
 def forceNotPresent() {
-	sendEvent(name: "presence", value: "not present")
+	sendEvent(name: "presence", value: "not present", isStateChange: true)
 }
 
 def parse(String description) {
