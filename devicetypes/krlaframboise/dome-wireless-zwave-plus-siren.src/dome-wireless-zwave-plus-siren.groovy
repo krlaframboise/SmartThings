89361f72ddedfb84c471f23d4d40465ae5453760
Kevin LaFramboise <klaframboise@comcast.net>
2016-12-31 23:45:45 -0500

    - Initial Release

--- devicetypes/krlaframboise/dome-wireless-zwave-plus-siren.src/dome-wireless-zwave-plus-siren.groovy
+++ devicetypes/krlaframboise/dome-wireless-zwave-plus-siren.src/dome-wireless-zwave-plus-siren.groovy
@@ -5,7 +5,7 @@
  *  Author: 
  *    Kevin LaFramboise (krlaframboise)
  *
- *  URL to documentation:  
+ *  URL to documentation:  https://community.smartthings.com/t/release-dome-wireless-z-wave-plus-siren/71091?u=krlaframboise
  *    
  *
  *  Changelog:
